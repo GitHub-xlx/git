89ee75db0bb0d1f0de5ded69dde791de3a2ae098
stubborn <2777631526@qq.com>
2020-02-14 21:46:48 +0800

    测试ben的git

--- myboot/src/main/java/com/xlx/MybootApplication.java
+++ myboot/src/main/java/com/xlx/MybootApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MybootApplication
 {
 
+	/**
+	 * 子盛你的输入法
+	 * @param args 出卖你
+	 */
+
 	public static void main(String[] args)
 	{
 		SpringApplication.run(MybootApplication.class, args);
